dda678c15992acadec5c6303403fedfc7e026ca1
Luca Meyer <kiste@Lucas-MBP-2.home>
2022-05-30 12:56:49 +0300

    Change config and deploy

--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  basePath: process.env.NODE_ENV === 'production' ? '' : 'portfolio',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '' : 'portfolio',
-  images: {
-    loader: "custom"
-  }
-}
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
 
-module.exports = nextConfig
+module.exports = withPlugins([
+  [optimizedImages, {
+    mozjpeg: {
+      quality: 80,
+    },
+    pngquant: {
+      speed: 3,
+      strip: true,
+      verbose: true,
+    },
+    imagesPublicPath: process.env.REPO_PREFIX,
+  }],
+
+  {
+    reactStrictMode: true,
+    basePath: process.env.REPO_PREFIX,
+    assetPrefix: process.env.REPO_PREFIX,
+  }
+]);
