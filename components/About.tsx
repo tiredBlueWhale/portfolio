47d55e1963a577770b3a576fcdf2ca4009493f5c
Luca Meyer <kiste@Lucas-MBP-2.home>
2022-05-30 13:53:25 +0300

    Fix build error

--- components/About.tsx
+++ components/About.tsx
@@ -10,8 +10,7 @@ export const About = () => {
                 <Animation refViewport={refAbout} type="scroll-horizontal" speed={.8}>
                     <p className="text-white text-4xl sm:text-6xl text-center">
                         Hello | Hallo | Moi | 안녕 | やあ | Hola | Oi | 你好 | Salve<br />
-                        {/* to the Portofolio <br /> */}
-                        Let's dive in ^_^
+                        Let&apos;s dive in ^_^
                     </p>
                 </Animation>
             </div>
