e2d1910180e33064ca3e2798ff174a8a37cccdf5
tiredBlueWhale <71780728+tiredBlueWhale@users.noreply.github.com>
2022-06-03 07:58:16 +0000

    Deploying to gh-pages from @ tiredBlueWhale/portfolio@7cf3081d9ca566644cb9b59971ddd007775a34ff 🚀

--- _next/static/V8My_N7eHva8_O1IHCxCh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/996-9e3c12b77542c098.js","static/chunks/pages/index-be87124f053a982d.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
