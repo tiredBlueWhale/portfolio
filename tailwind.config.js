b91a4aaa53d3bffdbb5d4fc24c9097c36a9d6bae
tiredBlueWhale <tired.blue.whale@gmail.com>
2023-08-16 19:13:31 +0300

    Change vh to dvh

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
+      height: {
+        screen: ['100vh /* fallback for Opera, IE and etc. */', '100dvh'],
+      },
       keyframes: {
         'fade-in': {
           '0%': { opacity: '0' },
