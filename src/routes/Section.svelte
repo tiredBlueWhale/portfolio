130839e70176c9404c5b3393e03d24613e30ec66
tiredBlueWhale <tired.blue.whale@gmail.com>
2023-08-16 18:22:11 +0300

    Change to IntersectionObserver

--- src/routes/Section.svelte
+++ src/routes/Section.svelte
@@ -1,4 +1,6 @@
 <script>
+	import { onMount } from 'svelte';
+
 	export let zIndex = '-z-10';
 	/**
 	 * @type {number}
@@ -8,7 +10,7 @@
 	/**
 	 * @type {number}
 	 */
-	let scrollY;
+	// let scrollY;
 
 	/**
 	 * @type {HTMLElement}
@@ -20,15 +22,35 @@
 	 */
 	let sectionPlaceholder;
 
-	$: {
-		if (section !== undefined && sectionPlaceholder !== null) {
-			const height = innerHeight + sectionPlaceholder.offsetTop - scrollY;
-			section.style.height = (height > 0 ? height : 0) + 'px';
+	// $: {
+	// 	if (section !== undefined && sectionPlaceholder !== null) {
+	// 		const height = innerHeight + sectionPlaceholder.offsetTop - scrollY;
+	// 		section.style.height = (height > 0 ? height : 0) + 'px';
+	// 	}
+	// }
+	onMount(() => {
+		if (typeof IntersectionObserver !== 'undefined') {
+			const threshold = Array.from({ length: 101 }, (_, i) => i / 100);
+			console.log(threshold);
+			const options = { threshold };
+			const observer = new IntersectionObserver((entries) => {
+				entries.forEach((entry) => {
+					console.log(entry.intersectionRatio);
+					const height = `${innerHeight * entry.intersectionRatio}px`;
+					console.log(height);
+					section.style.height = height;
+					console.log(section.style.height);
+				});
+			}, options);
+			observer.observe(sectionPlaceholder);
+
+			return () => observer.unobserve(sectionPlaceholder);
 		}
-	}
+	});
 </script>
 
-<svelte:window bind:innerHeight bind:scrollY />
+<!-- <svelte:window bind:innerHeight bind:scrollY /> -->
+<svelte:window bind:innerHeight />
 
 <section
 	class="fixed top-0 right-0 left-0 overflow-clip {zIndex}"
@@ -40,4 +62,4 @@
 	</div>
 </section>
 
-<div class="h-[110vh]" bind:this={sectionPlaceholder} />
+<div class="h-[100vh]" bind:this={sectionPlaceholder} />
