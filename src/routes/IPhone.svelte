51fadfb94e476554523825b2119c72f80b954522
tiredBlueWhale <tired.blue.whale@gmail.com>
2023-08-17 21:20:05 +0300

    iOS test

--- src/routes/IPhone.svelte
+++ src/routes/IPhone.svelte
@@ -4,8 +4,11 @@
 	export let src = stayInside;
 </script>
 
-<div
+<!-- <div
 	class="w-[150px] md:w-[200px] lg:w-[250px] xl:w-[300px] 2xl:w-[350px] h-[300px] md:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px] relative border-[7px] md:border-[9px] lg:border-[14px] rounded-[1.25rem] lg:rounded-[2.5rem] drop-shadow-2xl border-gray-800 bg-gray-800"
+> -->
+<div
+	class="w-[150px] md:w-[200px] lg:w-[250px] xl:w-[300px] 2xl:w-[350px] h-[300px] md:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]"
 >
 	<!-- <div class="w-[150px] h-[300px]"> -->
 	<!-- <div
