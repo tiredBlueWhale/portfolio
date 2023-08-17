e564251592e720e80fa89f43a5c80c630056beca
tiredBlueWhale <tired.blue.whale@gmail.com>
2023-08-17 20:57:05 +0300

    iOS test

--- src/routes/IPhone.svelte
+++ src/routes/IPhone.svelte
@@ -7,7 +7,7 @@
 <div
 	class="w-[150px] md:w-[200px] lg:w-[250px] xl:w-[300px] 2xl:w-[350px] h-[300px] md:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px] relative border-[7px] md:border-[9px] lg:border-[14px] rounded-[1.25rem] lg:rounded-[2.5rem] drop-shadow-2xl border-gray-800 bg-gray-800"
 >
-	<div
+	<!-- <div
 		class="absolute h-[4%] w-[24%] top-[1.5%] rounded-[2rem] left-1/2 -translate-x-1/2 bg-gray-800"
 	/>
 	<div class="absolute h-[7.5%] w-[1%] -left-[6%] top-[20%] rounded-l-lg bg-gray-800" />
@@ -15,8 +15,8 @@
 	<div class="absolute h-[10.5%] w-[1%] -right-[6%] top-[23%] rounded-r-lg bg-gray-800" />
 	<div
 		class="rounded-[1rem] lg:rounded-[2rem] overflow-hidden w-full h-full bg-white dark:bg-gray-800"
-	>
-		<img {src} class="w-auto h-full object-cover" alt="iPhone" />
-		<!-- <img {src} class="hidden dark:block w-auto h-full object-cover" alt="iPhone" /> -->
-	</div>
+	> -->
+	<img {src} class="w-auto h-full object-cover" alt="iPhone" />
+	<!-- <img {src} class="hidden dark:block w-auto h-full object-cover" alt="iPhone" /> -->
+	<!-- </div> -->
 </div>
--- src/routes/Laptop.svelte
+++ src/routes/Laptop.svelte
@@ -1,20 +1,20 @@
 <script>
-	export let src = 'https://flowbite.s3.amazonaws.com/docs/device-mockups/laptop-screen.png';
+	// export let src = 'https://flowbite.s3.amazonaws.com/docs/device-mockups/laptop-screen.png';
 </script>
 
 <div class="flex flex-col justify-center w-[300px] md:w-[400px] lg:w=[500px] xl:w-[800px]">
-	<div class="h-[94%] self-center">
+	<!-- <div class="h-[94%] self-center">
 		<div
 			class="h-full w-full border-[12px] border-gray-800 dark:border-gray-800 bg-gray-800 rounded-t-xl"
 		>
-			<div class="rounded-lg overflow-hidden w-full h-full bg-white dark:bg-gray-700">
-				<img {src} class="rounded-lg w-full h-auto object-cover object-center" alt="Laptop" />
-			</div>
+			<div class="rounded-lg overflow-hidden w-full h-full bg-white dark:bg-gray-700"> -->
+	<!-- <img {src} class="rounded-lg w-full h-auto object-cover object-center" alt="Laptop" /> -->
+	<!-- </div>
 		</div>
 	</div>
 	<div
 		class="flex justify-center items-start h-[6%] border-gray-900 dark:bg-gray-700 rounded-b-xl rounded-t-sm"
 	>
 		<div class="w-[15%] h-1/2 rounded-b-xl bg-gray-800" />
-	</div>
+	</div> -->
 </div>
