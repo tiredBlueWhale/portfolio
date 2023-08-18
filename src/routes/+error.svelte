e8b512201be8f1e4ce4b54ebd6318f432be08bd2
tiredBlueWhale <tired.blue.whale@gmail.com>
2023-08-18 14:53:59 +0300

    Remove Error

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,3 +1,4 @@
-<section class="h-screen flex justify-center items-center bg-neutral-600">
-	<h1 class="lg:text-5xl">Under Construction</h1>
+<section class="h-screen flex flex-col justify-center items-center bg-neutral-600">
+	<h1 class="text-4xl lg:text-6xl">Under Construction</h1>
+	<h2 class="text-lg lg:text-2xl">Please come back to a later time</h2>
 </section>
--- src/routes/[slug]/privacy/+page.js
+++ src/routes/[slug]/privacy/+page.js
@@ -34,5 +34,5 @@ export function load({ params }) {
         return { component };
     }
 
-    throw error(404, 'Not found');
+    // throw error(404, 'Not found');
 }
