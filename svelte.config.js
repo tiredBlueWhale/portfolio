b05fb3fd80dcff6f51d2b230ba0b5eaf209914ee
tiredBlueWhale <tired.blue.whale@gmail.com>
2023-08-19 20:32:56 +0300

    Change svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,16 +10,9 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			// https://kit.svelte.dev/docs/adapter-static#usage
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		}),
+		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+			base: dev ? '' : process.env.BASE_PATH,
 		}
 	},
 	preprocess: vitePreprocess()
