2668e095e76426738af683ffe862cd434c5059a8
tiredBlueWhale <tired.blue.whale@gmail.com>
2024-01-31 23:16:47 +0200

    Fix build error

--- src/lib/components/TextFooter.svelte
+++ src/lib/components/TextFooter.svelte
@@ -15,7 +15,7 @@
 <div class="flex gap-4">
 	<slot />
 	<a class="text-base lg:text-lg" href="https://github.com/tiredBlueWhale">GitHub </a>
-	<a class="text-base lg:text-lg" href={external ? path : '{base}/{path}/privacy'}
+	<a class="text-base lg:text-lg" href={external ? path : `${base}/${path}/privacy`}
 		>Privacy Policy
 	</a>
 </div>
--- svelte.config.js
+++ svelte.config.js
@@ -11,6 +11,18 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
+		// Might be needed
+		// prerender: {
+		// 	handleHttpError: ({ path, referrer, message }) => {
+		// 		// ignore deliberate link to shiny 404 page
+		// 		// if (path.includes('www.iubenda.com/privacy-policy')) {
+		// 		// 	return;
+		// 		// }
+
+		// 		// otherwise fail the build
+		// 		throw new Error(message);
+		// 	}
+		// },
 		paths: {
 			base: dev ? '' : process.env.BASE_PATH,
 		}
